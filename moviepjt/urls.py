7be84e3e44c5585acb2049900f4f1edb3bfa770a
ererink <yelkim0210@gmail.com>
2022-09-30 11:32:37 +0900

    index, new, detail 추가

---
+++ moviepjt/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = "moviepjt"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("new/", views.new, name="new"),
+    path("create/", views.create, name="create"),
+    path("detail/<int:pk>", views.detail, name="detail"),
+]
---
+++ moviepjt/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from .models import Review
+
+
+# Create your views here.
+def index(request):
+    reviews = Review.objects.all()
+
+    context = {
+        "reviews": reviews,
+    }
+
+    return render(request, "moviepjt/index.html", context)
+
+
+def new(request):
+
+    return render(request, "moviepjt/new.html")
+
+
+def create(request):
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+
+    # DB에 저장
+    Review.objects.create(title=title, content=content)
+
+    context = {
+        "title": title,
+        "content": content,
+    }
+    return redirect("moviepjt:index")
+
+
+def detail(request, pk):
+    review = Review.objects.get(pk=pk)
+    context = {
+        "review": review,
+    }
+    return render(request, "moviepjt/detail.html", context)
